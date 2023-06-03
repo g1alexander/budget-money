f0d64a6b5d11922c5f45965b3fadef6d1482e3d5
g1alexander <alexander.granados.dev@gmail.com>
2023-06-03 12:34:43 -0500

    test: prisma performance

---
+++ src/Hola.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+async function postData() {
+  const feed = await fetch(`${process.env.VERCEL_URL}/api`, {
+    method: "POST",
+    body: JSON.stringify({ name: "John Doe" }),
+  }).then((res) => res.json());
+
+  console.log(feed);
+  return {
+    feed,
+  };
+}
+
+export default function Hola() {
+  const hola = async () => {
+    console.log("hola");
+
+    await postData();
+  };
+  return <button onClick={hola}>hjoasdkas</button>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,37 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
+import Hola from "@/Hola";
 
-export default function Home() {
+async function getData() {
+  const feed = await fetch(`${process.env.VERCEL_URL}/api`).then((res) =>
+    res.json()
+  );
+
+  return {
+    feed,
+  };
+}
+
+export default async function Home() {
+  const { feed } = await getData();
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
+          {JSON.stringify(feed)}
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.tsx</code>
         </p>
+
+        <Hola />
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -91,5 +107,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
