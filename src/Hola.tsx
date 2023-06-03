ba2a3845717eec74b8602c027c9387054b4378dd
g1alexander <alexander.granados.dev@gmail.com>
2023-06-03 13:22:59 -0500

    fix: validate

--- src/Hola.tsx
+++ src/Hola.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 async function postData() {
-  const feed = await fetch(`${process.env.API_URL}/api`, {
+  const feed = await fetch(`${process.env.API_URL ?? ""}/api`, {
     method: "POST",
     body: JSON.stringify({ name: "John Doe" }),
   }).then((res) => res.text());
